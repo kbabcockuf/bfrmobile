91488342383e55dc9faad3b46c83abd457d31573
Austin Brown <austin@dragondev.com>
2015-01-20 10:53:38 -0700

    Add missing js sources

---
+++ www/js/animation.js
@@ -0,0 +1,19 @@
+angular.module("BFRMobile.animation", [])
+    /**
+     * Grow from zero height.
+     */
+    .animation('.bfr-animate-grow', function() {
+        return {
+            enter: function(el, done) {
+                el.css('opacity', 0);
+                var autoHeight = el.outerHeight();
+                el.css('height', 0);
+                el.animate({
+                    opacity: 1,
+                    height: autoHeight}, 300, 'swing', done);
+            },
+            leave: function(el, done) {
+                el.animate({opacity: 0, height: 0}, 300, 'swing', done);
+            }
+        };
+    });
